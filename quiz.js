750113e591748ca3b3486940425a6b066d63e725
elisiosantos <elisio13cantil@gmail.com>
2020-12-16 23:33:19 -0300

    terceiro

---
+++ quiz.js
@@ -0,0 +1,49 @@
+function pergunta1(){
+    var n1=document.getElementsByName('n1')
+    var n2=document.getElementById('asdf')
+   
+
+    var n4=document.getElementsByName('n2')
+    var n5=document.getElementById('asdfg')
+
+    
+    var n6=document.getElementsByName('n3')
+    var n7=document.getElementById('asdfghj')
+
+    var n8=document.getElementsByName('n4')
+    var n9=document.getElementById('asdfghjk')
+
+    
+    var n10=document.getElementsByName('n5')
+    var n11=document.getElementById('asdfghjkl')
+
+    if(n1[0].checked){
+        n2.style.color = "green";
+    }else{
+        n2.style.color = "green";
+    } if(n4[2].checked){
+        n5.style.color = "green";
+    }else{
+        n5.style.color = "green";
+    }if(n6[1].checked){
+        n7.style.color = "green";
+    }else{
+        n7.style.color = "green";
+    }if(n8[1].checked){
+        n9.style.color = "green";
+    }else{
+        n9.style.color = "green";
+    } if(n10[1].checked){
+        n11.style.color = "green";
+    }else{
+        n11.style.color = "green";
+    }
+
+
+}
+
+
+
+
+
+
